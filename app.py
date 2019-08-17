8e97a01f02f4cd1f7989338d76b377991aa2de89
prodproject <prod.projectmanager@gmail.com>
2019-08-17 11:22:29 +0530

    no chnages

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET','POST'])
+def getServerStatus():
+    database = os.environ.get('DATABASE_URL',0)
+    environment = os.environ.get('ENVIRONMENT',1)
+    url = os.environ.get('URL',2)
+    status = os.environ.get('STATUS',3)
+    response = database+"\n"+environment+"\n"+url+"\n"+status
+    return response
+
 
 @app.route('/workerMain', methods=['GET'])
 def getWorker():
