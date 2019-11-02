ff5dc4522600b8a30405ad17e121756f26737c20
prodproject <prod.projectmanager@gmail.com>
2019-11-02 14:47:56 +0530

    make it better

--- Test/loginModuleTest.py
+++ Test/loginModuleTest.py
@@ -1,9 +1,7 @@
 from CommonCode.convertJSONTOPb import ConvertJSONToPb
-from Services.loginModuleService import LoginModuleService
 from Services.loginService import LoginService
 from protobuff.login_pb2 import LoginRequestPb
 
-m_login = LoginModuleService()
 m_loginService = LoginService()
 m_loginReq = LoginRequestPb()
 m_loginReq.login.CopyFrom(m_loginService.get(id="kZ"))
