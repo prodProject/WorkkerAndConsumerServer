e9ab2a62973215349441eca0df561df3ae3756bc
prodproject <prod.projectmanager@gmail.com>
2019-11-03 13:11:22 +0530

    make it better

--- CommonCode/passwordHashOrDehashHelper.py
+++ CommonCode/passwordHashOrDehashHelper.py
@@ -4,7 +4,8 @@ import bcrypt
 class PasswordHasherOrDeHasher:
 
     def getHashFromPassword(self, password):
-        return bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
+        pass_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return pass_hash.decode('utf-8')
 
     def getPasswordFromHash(self, actualPassword, hashedPassword):
-        return bcrypt.checkpw(actualPassword.encode('utf-8'), hashedPassword)
+        return bcrypt.checkpw(actualPassword.encode('utf-8'), hashedPassword.encode('utf-8'))
