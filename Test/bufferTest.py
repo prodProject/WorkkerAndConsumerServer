043c69ca8bd50ef988df7065747b8bc8884b808e
prodproject <prod.projectmanager@gmail.com>
2019-12-08 17:18:53 +0530

    no changes

--- Test/bufferTest.py
+++
@@ -1,8 +0,0 @@
-from google.protobuf.json_format import MessageToJson
-
-from Services.workerService import WorkerService
-
-m_worker = WorkerService()
-worker = m_worker.get(id="ku")
-worker.dbInfo.id = ""
-print(m_worker.update(id="ku",builder=worker))
