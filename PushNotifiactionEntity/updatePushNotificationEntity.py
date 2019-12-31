c786a5ae52101289120ef3eb0871317a633b3c5e
prodproject <prod.projectmanager@gmail.com>
2019-12-31 15:53:36 +0530

    make it better

--- PushNotifiactionEntity/updatePushNotificationEntity.py
+++ PushNotifiactionEntity/updatePushNotificationEntity.py
@@ -7,6 +7,7 @@ from Comparetor.pushNotificationComparetor import PushNotificationComparetor
 from Enums.databaseTables import Tables
 from Helper.entityHelper import EntityHelper
 from PushNotifiactionEntity.getPushNotificationEntity import GetPushNotificationEntity
+from Services.workerService import WorkerService
 
 
 class States(Enum):
@@ -14,11 +15,14 @@ class States(Enum):
     GET_ENTITY_ID = 1,
     COMPARE_PB = 2
     UPDATE_ENTITY_ID = 3,
-    DONE = 4,
+    GET_WORKER = 4,
+    UPDATE_PUSHNOTIFICATION_IN_WORKER = 5,
+    DONE = 6,
 
 
 class UpdatePushNotificationEntity:
     m_helper = EntityHelper()
+    m_workerService = WorkerService()
     m_getEntity = GetPushNotificationEntity()
     m_queryExecutor = QueryExecuter()
     m_converterPbToJson = ConvertPbToJSON()
@@ -27,6 +31,7 @@ class UpdatePushNotificationEntity:
     oldPb = None
     builder = None
     id = None
+    m_worker = None;
 
     def start(self, id, builder):
         self.id = id
@@ -50,6 +55,19 @@ class UpdatePushNotificationEntity:
     def updateEntity(self):
         newPb = self.m_queryExecutor.update(id=self.id, builder=self.builder, table=Tables.PUSH_NOTIFICATON.name)
         self.builder = newPb
+        self.controlFlow(currentState=States.GET_WORKER)
+
+    def getWorker(self):
+        worker = self.m_workerService.get(id=self.builder.workerRef.dbInfo.id)
+        if (worker != None):
+            self.m_worker = worker;
+        self.controlFlow(currentState=States.UPDATE_PUSHNOTIFICATION_IN_WORKER)
+
+    def updateWorker(self):
+        self.m_worker.pushNotificationRef.name.canonicalName = self.builder.tokenId;
+        worker = self.m_workerService.update(id=self.m_worker.dbInfo.id, builder=self.m_worker)
+        if (worker != None):
+            self.m_worker = worker;
         self.controlFlow(currentState=States.DONE)
 
     def controlFlow(self, currentState):
@@ -59,5 +77,9 @@ class UpdatePushNotificationEntity:
             self.comaprePb()
         elif (currentState == States.UPDATE_ENTITY_ID):
             self.updateEntity()
+        elif (currentState == States.GET_WORKER):
+            self.getWorker()
+        elif (currentState == States.UPDATE_PUSHNOTIFICATION_IN_WORKER):
+            self.updateWorker()
         elif (currentState == States.DONE):
             self.done()
